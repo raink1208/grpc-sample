d183af1530872a077998a21f63400dbd1679eb81
raink1208 <58714901+raink1208@users.noreply.github.com>
2022-01-14 23:16:38 +0900

    applicationのmainClassを設定

--- grpc-sample-server/build.gradle.kts
+++ grpc-sample-server/build.gradle.kts
@@ -15,19 +15,6 @@ dependencies {
     runtimeOnly("io.grpc:grpc-netty:${rootProject.ext["grpcVersion"]}")
 }
 
-tasks.register<JavaExec>("HelloWorldServer") {
-    dependsOn("classes")
-    classpath = sourceSets["main"].runtimeClasspath
-    mainClass.set("com.github.raink1208.grpcsample.serverHelloWorldServerKt")
-}
-
-val helloWorldServerStartScripts = tasks.register<CreateStartScripts>("helloWorldServerStartScripts") {
+application {
     mainClass.set("com.github.raink1208.grpcsample.server.HelloWorldServerKt")
-    applicationName = "hello-world-server"
-    outputDir = tasks.named<CreateStartScripts>("startScripts").get().outputDir
-    classpath = tasks.named<CreateStartScripts>("startScripts").get().classpath
-}
-
-tasks.named("startScripts") {
-    dependsOn(helloWorldServerStartScripts)
 }
