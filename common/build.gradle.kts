741bd392223e345d76080e90d3dd455c53e31404
raink1208 <58714901+raink1208@users.noreply.github.com>
2022-01-14 23:17:46 +0900

    生成されたファイルをsourceSetに追加

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -54,3 +54,14 @@ protobuf {
         }
     }
 }
+
+sourceSets {
+    main {
+        java {
+            srcDir("build/generated/source/proto/main/grpc")
+            srcDir("build/generated/source/proto/main/grpckt")
+            srcDir("build/generated/source/proto/main/java")
+            srcDir("build/generated/source/proto/main/kotlin")
+        }
+    }
+}
