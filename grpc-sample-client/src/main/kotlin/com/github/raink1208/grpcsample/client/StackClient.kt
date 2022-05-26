7667ac0b6fdcd1c40e75eb6d8e042b62ab09a6c9
raink1208 <58714901+raink1208@users.noreply.github.com>
2022-05-27 00:23:29 +0900

    サーバーとクライアントの実装

--- grpc-sample-client/src/main/kotlin/com/github/raink1208/grpcsample/client/StackClient.kt
+++ grpc-sample-client/src/main/kotlin/com/github/raink1208/grpcsample/client/StackClient.kt
@@ -1,4 +1,51 @@
 package com.github.raink1208.grpcsample.client
 
-class StackClient {
+import com.github.raink1208.grpcsample.protos.StackSampleGrpcKt
+import com.github.raink1208.grpcsample.protos.stackData
+import com.google.protobuf.Empty
+import io.grpc.ManagedChannel
+import io.grpc.ManagedChannelBuilder
+import java.io.Closeable
+import java.util.concurrent.TimeUnit
+
+class StackClient(private val channel: ManagedChannel, val player: String): Closeable {
+    private val stub: StackSampleGrpcKt.StackSampleCoroutineStub = StackSampleGrpcKt.StackSampleCoroutineStub(channel)
+
+    suspend fun addData(x: Int, y: Int) {
+        val request = stackData {
+            name = player
+            this.x = x
+            this.y = y
+        }
+        stub.stack(request)
+    }
+
+    suspend fun getStackedData() {
+        val response = stub.getAll(Empty.getDefaultInstance())
+        println("count: " + response.dataCount)
+        for (i in response.dataList) {
+            println(i.name + ":" + i.x + ":" + i.y)
+        }
+    }
+
+    override fun close() {
+        channel.shutdown().awaitTermination(5, TimeUnit.SECONDS)
+    }
+}
+
+suspend fun main() {
+    val port = 50051
+    val channel = ManagedChannelBuilder.forAddress("localhost", port).usePlaintext().build()
+    val name = readLine() ?: "test"
+    val client = StackClient(channel, name)
+
+    while (true) {
+        val input = readLine() ?: ""
+        if (input == "") break
+        val data = input.split(" ").map { it.toInt() }
+        client.addData(data[0], data[1])
+    }
+    client.getStackedData()
+
+    client.close()
 }
--- grpc-sample-server/src/main/kotlin/com/github/raink1208/grpcsample/server/StackServer.kt
+++ grpc-sample-server/src/main/kotlin/com/github/raink1208/grpcsample/server/StackServer.kt
@@ -1,4 +1,58 @@
 package com.github.raink1208.grpcsample.server
 
-class StackServer {
+import com.github.raink1208.grpcsample.protos.StackData
+import com.github.raink1208.grpcsample.protos.StackSampleGrpcKt
+import com.github.raink1208.grpcsample.protos.StackedData
+import com.github.raink1208.grpcsample.protos.stackedData
+import com.google.protobuf.Empty
+import io.grpc.Server
+import io.grpc.ServerBuilder
+
+class StackServer(private val port: Int) {
+    val server: Server = ServerBuilder
+        .forPort(port)
+        .addService(StackService())
+        .build()
+
+    fun start() {
+        server.start()
+        println("Server started, listening on $port")
+        Runtime.getRuntime().addShutdownHook(
+            Thread {
+                println("*** shutting down gRPC server since JVM is shutting down")
+                this@StackServer.stop()
+                println("*** server shut down")
+            }
+        )
+    }
+
+    private fun stop() {
+        server.shutdown()
+    }
+
+    fun blockUntilShutdown() {
+        server.awaitTermination()
+    }
+
+    private class StackService: StackSampleGrpcKt.StackSampleCoroutineImplBase() {
+        val stackedDataList = mutableListOf<StackData>()
+        override suspend fun stack(request: StackData): Empty {
+            println(request.name + ":" + "[x:${request.x}, y:${request.y}]")
+            stackedDataList.add(request)
+            return Empty.getDefaultInstance()
+        }
+
+        override suspend fun getAll(request: Empty): StackedData {
+            return stackedData {
+                data.addAll(stackedDataList)
+            }
+        }
+    }
+}
+
+fun main() {
+    val port = 50051
+    val server = StackServer(port)
+    server.start()
+    server.blockUntilShutdown()
 }
